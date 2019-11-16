b7881ba8a3467e5c66408e8f3aa8a927a2a2cb9b
Zac Heisey <zheisey@gmail.com>
2019-11-16 13:58:16 -0500

    complete footer

---
+++ main.js
@@ -0,0 +1,6 @@
+// Auto-update footer copyright date
+
+var copyrightDate = document.getElementById('copright-date');
+var currentYear = new Date().getFullYear();
+
+copyrightDate.innerText = currentYear;
